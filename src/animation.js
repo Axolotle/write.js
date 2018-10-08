df3d516869a67f3ffdb9f629260fc923ec751349
Axolotle <nchesnais@mailoo.org>
2018-10-08 13:49:08 +0200

    add speed change to animation

--- src/animation.js
+++ src/animation.js
@@ -1,11 +1,10 @@
 import { Tag } from "./parser/parser.js";
-import { sleep } from "./utils.js";
+import { sleep, has } from "./utils.js";
 
-export async function animate(txt, display, split="", speed=40, startX=0, startY=0) {
+export async function animate(txt, display, split="", speed=70, startX=0, startY=0) {
     var domNodes = document.getElementById(display.nodeName).children;
     startY += display.padding.y;
     startX += display.padding.x;
-    console.log(domNodes);
     var nodes = [];
     nodes.last = () => {
         return this[this.length - 1];
@@ -20,7 +19,7 @@ export async function animate(txt, display, split="", speed=40, startX=0, startY
 
         doc.appendChild(document.createTextNode(prevTxt.slice(0, startX)));
         doc.appendChild(receiver);
-        doc.appendChild(rest)
+        doc.appendChild(rest);
         doc.appendChild(document.createTextNode(prevTxt.slice(prevTxt.length - display.padding.x)));
         domNodes[l+startY].replaceWith(doc);
 
@@ -28,36 +27,35 @@ export async function animate(txt, display, split="", speed=40, startX=0, startY
         var actualDomNode;
 
 
-        await displayContent(txt[l], receiver, rest);
+        speed = await displayContent(txt[l], receiver, rest, speed);
 
     }
 }
 
-export function displayContent(emitter, receiver, rest) {
+export function displayContent(emitter, receiver, rest, speed) {
     return new Promise (async (resolve) => {
         for (let node of emitter.nodes) {
             if (node instanceof Tag) {
                 let newReceiver = document.createElement(node.nodeName);
                 receiver.appendChild(newReceiver);
-                await displayContent(node, newReceiver, rest);
+                speed = await displayContent(node, newReceiver, rest, speed);
             } else if (typeof node === "object") {
-                if (node.pause) {
+                if (has(node, "pause")) {
                     await sleep(node.pause * 1000);
+                } else if (has(node, "speed")) {
+                    speed += node.speed * -10;
                 }
             } else {
                 let sentence = node.split("");
                 let newReceiver = document.createTextNode("");
                 receiver.appendChild(newReceiver);
                 for (let glyph of sentence) {
-                    receiver.textContent += glyph;
+                    newReceiver.textContent += glyph;
                     rest.textContent = rest.textContent.slice(1);
-                    await sleep(40);
+                    await sleep(speed);
                 }
             }
         }
-        resolve();
+        resolve(speed);
     });
 }
-
-
-// prevoir sur tag/line/etc. des fonction remove first/remove n
