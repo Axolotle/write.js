b760a6fad032f218d4536e68073420322b380f02
Axolotle <nchesnais@mailoo.org>
2018-10-11 14:52:01 +0200

    Add temp animated cleaning function

--- src/animation.js
+++ src/animation.js
@@ -5,28 +5,6 @@ export async function animate(txt, display, split="", speed=70, startX=0, startY
     var prevLines = display.lines;
 
     for (let line of txt) {
-        function getTextNode (node) {
-            if(node.constructor === Text) {
-                return node;
-            } else if (!node.hasChildNodes()) {
-                let next;
-                if (node.nextSibling) {
-                    next = node.nextSibling;
-                } else {
-                    next = node.parentNode;
-                }
-                node.remove()
-                return getTextNode(next);
-            } else {
-                if (node.classList.contains("line")) {
-                    return getTextNode(rest.first());
-                } else {
-                    return getTextNode(node.firstChild);
-                }
-
-            }
-        }
-
         let prevLine = prevLines[startY];
         let rest = Array.from(prevLine.childNodes);
         rest.first = function () {
@@ -41,11 +19,11 @@ export async function animate(txt, display, split="", speed=70, startX=0, startY
             } else {
                 let parent = this.textNode.parentNode;
                 this.textNode.remove();
-                this.textNode = getTextNode(parent);
+                this.textNode = getTextNode(parent, rest);
                 this.removeGlyph();
             }
         }
-        rest.textNode = getTextNode(rest[0]);
+        rest.textNode = getTextNode(rest[0], rest);
 
         for (let node of line) {
             if (node instanceof Tag) {
@@ -76,7 +54,6 @@ export async function animate(txt, display, split="", speed=70, startX=0, startY
 
 export function displayContent(emitter, receiver, rest, speed) {
     return new Promise (async (resolve) => {
-        console.log(emitter);
         for (let node of emitter.nodes) {
             if (node instanceof Tag) {
                 let newReceiver = node.emptyElement;
@@ -101,4 +78,60 @@ export function displayContent(emitter, receiver, rest, speed) {
         }
         resolve(speed);
     });
+}
+
+function getTextNode(node, rest) {
+    if(node.constructor === Text) {
+        return node;
+    } else if (!node.hasChildNodes()) {
+        let next;
+        if (node.nextSibling) {
+            next = node.nextSibling;
+        } else {
+            next = node.parentNode;
+        }
+        node.remove()
+        return getTextNode(next, rest);
+    } else {
+        if (node.classList.contains("line")) {
+            return getTextNode(rest.first(), rest);
+        } else {
+            return getTextNode(node.firstChild, rest);
+        }
+
+    }
+}
+
+export async function animatedClean(display, glyph=" ", speed=50) {
+    var prevLines = display.lines;
+    let width = display.width;
+
+    for (let prevLine of prevLines) {
+        let rest = Array.from(prevLine.childNodes);
+        rest.first = function () {
+            for (let node of this) {
+                if (node.textContent !== "") return node;
+            }
+        }
+        rest.removeGlyph = function () {
+            let t = this.textNode.textContent;
+            if (t.length > 0) {
+                this.textNode.textContent = t.slice(1);
+            } else {
+                let parent = this.textNode.parentNode;
+                this.textNode.remove();
+                this.textNode = getTextNode(parent, this);
+                this.removeGlyph();
+            }
+        }
+        rest.textNode = getTextNode(rest[0], rest);
+        let receiver = document.createTextNode("");
+        prevLine.insertBefore(receiver, rest[0]);
+        for (let i = 0; i < width; i++) {
+            receiver.textContent += glyph;
+            rest.removeGlyph();
+            await sleep(speed);
+        }
+
+    }
 }
